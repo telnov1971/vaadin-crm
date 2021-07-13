737261ca18730df6c09e248ac221cfff1d3d844e
Oleg Telnov <telnov1971@gmail.com>
2021-07-13 09:26:04 +0600

    CRM 1 steps (Greeting)

---
+++ src/main/java/com/vaadin/tutorial/crm/backend/GreetService.java
@@ -0,0 +1,18 @@
+package com.vaadin.tutorial.crm.backend;
+
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+
+@Service
+public class GreetService implements Serializable {
+
+    public String greet(String name) {
+        if (name == null || name.isEmpty()) {
+            return "Hello anonymous user";
+        } else {
+            return "Hello " + name;
+        }
+    }
+
+}
